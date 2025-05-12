0afa5a26a9638eaac41c03fc0b452049d760a4c3
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-05-12 14:08:51 +0100

    Update deploy-keyvaults.tf

--- scripts/terraform-scripts/deploy-keyvaults.tf
+++ scripts/terraform-scripts/deploy-keyvaults.tf
@@ -1,1 +1,18 @@
+resource "azurerm_key_vault" "kv" {
+  name                        = "myKeyVault123"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = true
+  soft_delete_enabled         = true
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = ["get", "set", "list", "delete"]
+    key_permissions    = ["get", "create", "list", "delete"]
+  }
+}
 
