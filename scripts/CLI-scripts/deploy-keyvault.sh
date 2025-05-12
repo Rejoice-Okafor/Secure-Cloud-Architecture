593f4fa56f7c31c17f871c8c55856fe181e9b966
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-05-12 14:04:06 +0100

    Update deploy-keyvault.sh
    This Azure CLI scripts deploys keyvaults

--- scripts/CLI-scripts/deploy-keyvault.sh
+++ scripts/CLI-scripts/deploy-keyvault.sh
@@ -1,1 +1,12 @@
+az keyvault create \
+  --name myKeyVault123 \
+  --resource-group MyResourceGroup \
+  --location eastus \
+  --sku standard
+
+az keyvault set-policy \
+  --name myKeyVault123 \
+  --object-id <YOUR_OBJECT_ID> \
+  --secret-permissions get list set delete \
+  --key-permissions get list create delete
 
