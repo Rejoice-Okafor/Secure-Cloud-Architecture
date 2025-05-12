92b7631793352e7e4e0698c21986565e3eda49f1
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-05-12 14:04:56 +0100

    Update deploy-sql.sh
    This script deploys sql servers and databases

--- scripts/CLI-scripts/deploy-sql.sh
+++ scripts/CLI-scripts/deploy-sql.sh
@@ -1,1 +1,13 @@
+az sql server create \
+  --name mySqlServer1234 \
+  --resource-group MyResourceGroup \
+  --location eastus \
+  --admin-user sqladmin \
+  --admin-password StrongP@ssw0rd!
+
+az sql db create \
+  --resource-group MyResourceGroup \
+  --server mySqlServer1234 \
+  --name myDatabase \
+  --service-objective Basic
 
