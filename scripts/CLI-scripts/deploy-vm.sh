42405d4ed7194a058081ee2640d065191b2b02d2
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-05-12 14:05:33 +0100

    Update deploy-vm.sh

--- scripts/CLI-scripts/deploy-vm.sh
+++ scripts/CLI-scripts/deploy-vm.sh
@@ -1,1 +1,9 @@
+az vm create \
+  --name myVm \
+  --resource-group MyResourceGroup \
+  --image Win2022Datacenter \
+  --admin-username azureuser \
+  --admin-password P@ssword1234! \
+  --size Standard_B2s \
+  --location eastus
 
