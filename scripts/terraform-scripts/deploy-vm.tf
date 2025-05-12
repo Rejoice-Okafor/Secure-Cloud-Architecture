06dbffee460651fa98cbba4d636bfb79b6926a5c
Rejoice Okafor <112861600+Rejoice-Okafor@users.noreply.github.com>
2025-05-12 14:11:08 +0100

    Update deploy-vm.tf

--- scripts/terraform-scripts/deploy-vm.tf
+++ scripts/terraform-scripts/deploy-vm.tf
@@ -1,1 +1,34 @@
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "vmNic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = "myVm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+  size                  = "Standard_B2s"
+
+  admin_username = "azureuser"
+  admin_password = "P@ssword1234!"
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
